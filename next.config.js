01ea38613b206e3fbeb00fb962e4ff195a90152e
MochaChoco <asdswj@gmail.com>
2026-01-13 15:42:01 +0900

    ci : 이미지 배포 처리 수정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   // GitHub Pages usually requires a basePath if it's a project repository (e.g., /my-repo)
   // Update this to '/your-repo-name' if deploying to a project page.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
   images: {
     unoptimized: true, // Required for next export

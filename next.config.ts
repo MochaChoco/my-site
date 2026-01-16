aaf1362e30798aac2c5661f02d07b2cc2709f08d
MochaChoco <asdswj@gmail.com>
2026-01-16 17:22:39 +0900

    ci: 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
-const isExport = process.env.NEXT_PUBLIC_EXPORT === "true";
+const isExport =
+  process.env.NEXT_PUBLIC_EXPORT === "true" ||
+  process.env.GITHUB_PAGES === "true";
 
 const nextConfig: NextConfig = {
   output: isExport ? "export" : undefined,
